30ce0e793a04dd74af0d4487f6fc89f4fa535b07
jagg26 <john.ale26@gmail.com>
2025-05-12 22:56:24 -0300

    Week 02 journal design

---
+++ week02/Journal/Journal.cs
@@ -0,0 +1,52 @@
+public class Journal
+{
+    private List<Entry> _entries;
+
+    public void AddEntry(Entry entry)
+    {
+        _entries.Add(entry);
+    }
+
+    public void DisplayAll()
+    {
+        foreach (var entry in _entries)
+        {
+            entry.Display();
+            Console.WriteLine();
+        }
+    }
+
+    public void SaveToFile(string filename)
+    {
+        using (StreamWriter writer = new StreamWriter(filename))
+        {
+            foreach (var entry in _entries)
+            {
+                writer.WriteLine($"{entry._date}|{entry._promptText}|{entry._entryText}");
+            }
+        }
+    }
+
+    public void LoadFromFile(string filename)
+    {
+        _entries = new List<Entry>();
+        using (StreamReader reader = new StreamReader(filename))
+        {
+            string line;
+            while ((line = reader.ReadLine()) != null)
+            {
+                var parts = line.Split('|');
+                if (parts.Length == 3)
+                {
+                    Entry entry = new Entry
+                    {
+                        _date = parts[0],
+                        _promptText = parts[1],
+                        _entryText = parts[2]
+                    };
+                    _entries.Add(entry);
+                }
+            }
+        }
+    }
+}
---
+++ week02/Journal/PromptGenerator.cs
@@ -0,0 +1,9 @@
+public class PromptGenerator
+{
+    public List<string> _prompts;
+
+    public static string GetRandomPrompt()
+    {
+        return "";
+    }
+}
